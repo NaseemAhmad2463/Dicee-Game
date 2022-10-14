86e983abf9cd57a1889967f21bf5335982ba2269
NaseemAhmad2463 <83572602+NaseemAhmad2463@users.noreply.github.com>
2022-10-14 22:02:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ document.getElementsByClassName("btn")[0].onclick = function(){
     var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
     document.querySelector(".img2").setAttribute("src", randomDiceImage2);
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 1 Wins!"
+        document.querySelector("h1").innerHTML = "Player 1 Wins!🎉"
     }
     
     else if (randomNumber1 < randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 2 Wins!"
+        document.querySelector("h1").innerHTML = "Player 2 Wins!🎉"
     }
     
     else {
